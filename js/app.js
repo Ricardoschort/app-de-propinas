1a3d67c58edf73a6c3026ce9015e1ee8ec287e08
Ricardoschort <rschortbogh@gmail.com>
2023-01-20 12:52:51 -0500

    finalizando la app
    implementamos funciones para dar por completado el proceso de actulizar propinas segun el pedido del cliente

--- js/app.js
+++ js/app.js
@@ -142,9 +142,12 @@ function changeCant(orders){
   }
 
   clear()
-  viewResum()
 
- 
+  if(customer.order.length){
+    viewResum()
+  }else{
+    viewMesaggeEntrie()
+  }
  
 }
 
@@ -155,6 +158,11 @@ function viewResum (){
 
   const resumContainer = document.createElement("DIV");
   resumContainer.classList.add("col-md-6","card","shadow","my-3","mx-3","p-3")
+
+  const heading = document.createElement("H3")
+  heading.classList.add("fw-bold","text-center","my-4")
+  heading.textContent = "Lista de pedidos"
+  resumContainer.appendChild(heading)
 
   //crear html de la mesa
   const mesaResum = document.createElement("P")
@@ -172,6 +180,7 @@ function viewResum (){
   hourResume.textContent = hour
   
   //añadirlos a sus respectivos container
+  mesaResum.appendChild(mesaSpan)
   houResume.appendChild(hourResume)
   mesaResum.appendChild(houResume)
   resumContainer.appendChild(mesaResum)
@@ -198,15 +207,26 @@ function viewResum (){
     cantli.classList.add("list-group-item")
     cantli.textContent = `Cantidad: $${cant}`
 
-    const hr = document.createElement("HR")
+    const subTotalLI = document.createElement("li");
+    subTotalLI.classList.add("list-group-item")
+    subTotalLI.textContent = `Subtotal: $${precio*cant}`
+
+    const btnDelete = document.createElement("BUTTON");
+    btnDelete.classList.add("btn","btn-danger");
+    btnDelete.textContent = "Eliminar pedido"
+
+    btnDelete.onclick = function(){
+      deleteOrder(id)
+    }
+
+    const hr = document.createElement("HR");
   
-
-
-
     // añadir al html
     group.appendChild(nombreLI)
     group.appendChild(priceLI)
     group.appendChild(cantli)
+    group.appendChild(subTotalLI)
+    group.appendChild(btnDelete)
     group.appendChild(hr)
 
  
@@ -214,6 +234,8 @@ function viewResum (){
     
   })
 
+  //llamar propinas
+  tips()
 }
 
 function clear (){
@@ -222,4 +244,169 @@ function clear (){
   while(content.firstChild){
     content.removeChild(content.firstChild)
   }
+}
+
+function deleteOrder(id){
+  
+  const {order} = customer
+  const result = order.filter(article => article.id !== id);
+  customer.order = [...result]
+
+  clear()
+  if(customer.order.length){
+    viewResum()
+  }else{
+    viewMesaggeEntrie()
+  }
+  
+  const productDelete = `#producto-${id}`
+  const inputDelete = document.querySelector(productDelete)
+  inputDelete.value = 0
+}
+
+function viewMesaggeEntrie(){
+  const contenidoRow = document.querySelector("#resumen .contenido")
+  const p = document.createElement("P");
+  p.classList.add("text-center");
+  p.textContent = "Agrega los elementos del pedido"
+
+  contenidoRow.appendChild(p);
+
+}
+
+// funcion para las propinas
+function tips(){
+  const resum = document.querySelector("#resumen .contenido");
+
+  const divTip = document.createElement("DIV");
+  divTip.classList.add("col-md-5","card","formulario","shadow","my-3","mx-3","p-3")
+
+  const heading = document.createElement("H3")
+  heading.classList.add("fw-bold","text-center","my-4")
+  heading.textContent = "Propinas"
+
+  // crear los radio para tomar el % de la propina al 10%
+  const radio10 = document.createElement("INPUT");
+  radio10.type = "radio";
+  radio10.value = "10"
+  radio10.name = "propina";
+  radio10.classList.add("form-check-input")
+
+  const radio10label = document.createElement("LABEL");
+  radio10label.textContent = "10%"
+  radio10label.classList.add("form-check-label");
+
+  const radio10Div = document.createElement("DIV")
+  radio10Div.classList.add("form-check")
+  radio10Div.appendChild(radio10);
+  radio10Div.appendChild(radio10label)
+  radio10.onclick = calculateTips
+
+   // crear los radio para tomar el % de la propina al 25%
+   const radio25 = document.createElement("INPUT");
+   radio25.type = "radio";
+   radio25.value = "25"
+   radio25.name = "propina";
+   radio25.classList.add("form-check-input");
+   radio25.onclick = calculateTips
+ 
+   const radio25label = document.createElement("LABEL");
+   radio25label.textContent = "25%"
+   radio25label.classList.add("form-check-label");
+ 
+   const radio25Div = document.createElement("DIV")
+   radio25Div.classList.add("form-check")
+   radio25Div.appendChild(radio25);
+   radio25Div.appendChild(radio25label);
+   radio25.onclick = calculateTips
+
+    // crear los radio para tomar el % de la propina al 50%
+    const radio50 = document.createElement("INPUT");
+    radio50.type = "radio";
+    radio50.value = "50"
+    radio50.name = "propina";
+    radio50.classList.add("form-check-input")
+    radio50.onclick = calculateTips
+  
+    const radio50label = document.createElement("LABEL");
+    radio50label.textContent = "50%"
+    radio50label.classList.add("form-check-label");
+  
+    const radio50Div = document.createElement("DIV")
+    radio50Div.classList.add("form-check")
+    radio50Div.appendChild(radio50);
+    radio50Div.appendChild(radio50label)
+
+
+
+  // añadirlo al div principal y al doom
+  divTip.appendChild(heading)
+  divTip.appendChild(radio10Div)
+  divTip.appendChild(radio25Div)
+  divTip.appendChild(radio50Div)
+
+  resum.appendChild(divTip);
+
+}
+
+
+  // agregar el valor de la propina
+
+function calculateTips(){
+  const {order}= customer
+  let subtotal
+
+  order.forEach(order =>{
+    subtotal = order.cant * order.precio
+  })
+  
+  const tipValue = document.querySelector(`[name="propina"]:checked`).value;
+  
+  let totalTips = ((subtotal*parseInt(tipValue))/100) 
+  let totalToPay = subtotal + totalTips
+  viewTips(subtotal,totalTips,totalToPay)
+}
+
+function viewTips(subtotal,propina,total){
+  const divTotalContainer = document.querySelector(".formulario");
+  const divTotal = document.createElement("DIV");
+  divTotal.classList.add("total")
+
+
+  const subtotalDiv = document.createElement("P")
+  subtotalDiv.textContent = "Subtotal: "
+  subtotalDiv.classList.add("fw-bold","pt-3")
+  const subtotalSpan = document.createElement("SPAN");
+  subtotalSpan.classList.add("fw-normal")
+  subtotalSpan.textContent = `$${subtotal}`
+  subtotalDiv.appendChild(subtotalSpan)
+
+
+  const tipDiv = document.createElement("P")
+  tipDiv.textContent = "Propina: "
+  tipDiv.classList.add("fw-bold",)
+  const tipSpan = document.createElement("SPAN");
+  tipSpan.classList.add("fw-normal")
+  tipSpan.textContent = `$${propina}`
+  tipDiv.appendChild(tipSpan)
+
+  const totalDiv = document.createElement("P")
+  totalDiv.textContent = "Valor a pagar: "
+  totalDiv.classList.add("fw-bold",)
+  const totalSpan = document.createElement("SPAN");
+  totalSpan.classList.add("fw-normal")
+  totalSpan.textContent = `$${total}`
+  totalDiv.appendChild(totalSpan)
+
+  const totalInfo = document.querySelector(".total")
+  if (totalInfo) {
+    totalInfo.remove()
+  }
+  divTotal.appendChild(subtotalDiv);
+  divTotal.appendChild(tipDiv);
+  divTotal.appendChild(totalDiv);
+  
+  
+  divTotalContainer.appendChild(divTotal);
+
 }
